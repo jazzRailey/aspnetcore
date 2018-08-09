203e304354cd22b60c21205fd9ea605c8df3cf27
Kenyo <henryandes@rocketmail.com>
2018-08-09 11:54:49 +0800

    Able to learn what is ASP.Net.Core  Razor pages Review your notes on cshtml file

---
+++ OdeToFood/Pages/Greeting.cshtml.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OdeToFood.Pages
+{
+    public class GreetingModel : PageModel
+    {
+        private readonly IGreeter _greeting;
+        public string CurrentGreeting { get; set; }
+        public GreetingModel(IGreeter greeting)
+        {
+            _greeting = greeting;
+        }
+        public void OnGet()
+        {
+            this.CurrentGreeting = _greeting.GetMessageOfTheDay();
+        }
+    }
+}
